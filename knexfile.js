8fa748b3959afb3af06031c2d9ca7bb6b33a200c
Vu <vuchau94@gmail.com>
2017-12-11 19:52:17 -0800

    Created catalog database tables

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+let environment = 'production' || `${process.env.NODE_ENV}`;
+console.log(`${process.env.NODE_ENV}`);
+module.exports = {
+	development: {
+		client: 'pg',
+		connection: 'postgres://postgres:rebase@localhost:5432/catalog',
+		migrations: {
+			directory: __dirname + `/catalog/database/migrations`
+		},
+		seeds: {
+			directory: __dirname + `/catalog/database/seeds`
+		}
+	},
+	production: {
+		client: 'pg',
+		connection: process.DATABASE_URL,
+		migrations: {
+			directory: __dirname + `/catalog/database/migrations`
+		},
+		seeds: {
+			directory: __dirname + `/catalog/database/seeds`
+		}
+	}
+}
